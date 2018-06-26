229242d30352b66f128f9033b7d1cb86f583e451
Dariusz Franczak <darius32@op.pl>
2018-06-26 22:08:01 +0100

    profile data fetched

--- app.js
+++ app.js
@@ -1,18 +1,28 @@
+//Initialise new object from Github class in github.js file
 const github = new Github();
 
-//UI elements
+//#### UI elements
+//Search input
 const searchUser = document.getElementById('search-user');
 
 
-// Event listener
+//Search input Event listener
 searchUser.addEventListener('keyup', function(e) {
   const userText = e.target.value;
-  
+
   if(userText !== '') {
     // Make http request
     github.getUser(userText)
     .then(data => {
-      console.log(data);
+      if(data.profile.message === 'Not Found') {
+        console.log('User not found');
+      } else {
+        console.log(data);
+      }
+      
     })
+  } else {
+    //clear profile
+    
   }
 });
--- github.js
+++ github.js
@@ -7,10 +7,10 @@ class Github {
   async getUser(user) {
     const profileResponse = await fetch(`https://api.github.com/users/${user}?client_id=${this.client_id}&client_secret=${this.client_secret}`);
   
-    const profile = await profileResponse.json();
+    const profileData = await profileResponse.json();
 
     return {
-      profile
+      profile: profileData
     }
   
   }
